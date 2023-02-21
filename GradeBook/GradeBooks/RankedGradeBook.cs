80a50e6d385840946821e109eb962899f583cf48
armin.patak <armin.patak@gmail.com>
2023-02-21 16:03:40 +0100

    Override RankedGradeBook's GetLetterGrade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -10,5 +10,24 @@ namespace GradeBook.GradeBooks
         {
             Type = Enums.GradeBookType.Ranked;
         }
+
+        public char GetLetterGrade(double averageGrade)
+        {
+            if(Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
+            }
+
+            switch (averageGrade) {
+                case 0.2: return'A';
+                case double i when i > 0.2 && i < 0.4: return 'B';
+                case double i when i > 0.4 && i < 0.6 : return 'C';
+                case double i when i > 0.6 && i < 0.8: return 'C';
+
+                default: return 'F';
+            }
+
+            
+        }
     }
 }
